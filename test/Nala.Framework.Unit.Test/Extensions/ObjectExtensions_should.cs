912180b9386707669cc3881cadca1cf2f7880eea
Seymour Poler <seymourpoler@gmail.com>
2019-04-08 19:08:17 +0200

    adding unit tests

---
+++ test/Nala.Framework.Unit.Test/Extensions/ObjectExtensions_should.cs
@@ -0,0 +1,31 @@
+﻿using Nala.Framework.Extensions;
+using Shouldly;
+using Xunit;
+
+namespace Nala.Framework.Unit.Test
+{
+    public class ObjectExtensions_should
+    {
+        [Fact]
+        public void return_true_when_is_not_null()
+        {
+            var user = new User();
+
+            var result = user.IsNotNull();
+            
+            result.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void return_true_when_is_null()
+        {
+            User user = null;
+
+            var result = user.IsNull();
+
+            result.ShouldBeTrue();
+        }
+        
+        class User{}
+    }
+}
